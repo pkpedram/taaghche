52a3d51aa9db56954f63474bfaf63ef83d4e1763
Pkpedram <pkpedram80@gmail.com>
2023-09-01 01:01:55 +0330

    creating the page

---
+++ pages/book/[id]/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import type { GetServerSideProps, NextPage } from "next";
+
+const BookDetailPage : NextPage = () => {
+  return (
+    <div>BookDetailPage</div>
+  )
+}
+
+export default BookDetailPage
